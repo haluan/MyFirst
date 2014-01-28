7c78b0be9305db87ef394441abf02a053671cd70
haluan <haluan.irsad@gmail.com>
2014-01-28 19:17:50 +0700

    Pembuatan class Produk

---
+++ src/Produk.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Haluan
+ */
+public class Produk {
+    private int id;
+    private String nama;
+    private double harga;
+    private String tanggalProduksi;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public double getHarga() {
+        return harga;
+    }
+
+    public void setHarga(double harga) {
+        this.harga = harga;
+    }
+
+    public String getTanggalProduksi() {
+        return tanggalProduksi;
+    }
+
+    public void setTanggalProduksi(String tanggalProduksi) {
+        this.tanggalProduksi = tanggalProduksi;
+    }
+    
+    
+}
